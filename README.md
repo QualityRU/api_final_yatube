ceff458eee816c808a54bc6c73fb24d157e1eae7
Александр Тарасов <qualityru@gmail.com>
2023-05-08 14:17:27 +0300

    Подготовка к сдаче 9 спринта
    Список изменений:
    - Заполнен README.md
    - Удалены неиспользуемые тесты и модели
    - В настройки проекта добавлен djoser, вход по по токену
    - В модели приложения posts добавлены недостающие модели
    - Сделаны миграции

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# api_final
-api final
+<div id="header" align="center">
+  <img src="https://media.giphy.com/media/M9gbBd9nbDrOTu1Mqx/giphy.gif" width="100"/>
+</div>
+<div id="badges" align="center">
+  <img src="https://img.shields.io/badge/Python-blue?style=for-the-badge&logo=python&logoColor=yellow" alt="Python"/>
+  <img src="https://img.shields.io/badge/Django-dark_green?style=for-the-badge&logo=django&logoColor=white" alt="Django"/>
+  <img src="https://img.shields.io/badge/API-blue?style=for-the-badge&logo=API&logoColor=white" alt="API"/>
+<a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&size=25&pause=500&color=F70000&center=true&vCenter=true&width=435&lines=Backend+Yatube" alt="Backend Yatube" /></a>
+</div>
+
+
+### Описание:
+Backend социальной сети, которая предназначен для создания и публикации личных дневников. Можно делиться своими публикациями и подписаться на любимых авторов.
+
+### Технологии используемые в проекте:
+- python==3.9.13
+- Django==3.2.16
+- pytest==6.2.4
+- pytest-pythonpath==0.7.3
+- pytest-django==4.4.0
+- djangorestframework==3.12.4
+- djangorestframework-simplejwt==4.7.2
+- Pillow==9.3.0
+- PyJWT==2.1.0
+- requests==2.26.0
+
+### Инструкция по запуску
+
+1) Клонировать репозиторий и перейти в него в командной строке:
+
+```
+git clone https://github.com/QualityRU/api_final_yatube.git
+```
+
+```
+cd api_final_yatube
+```
+
+2) Cоздать и активировать виртуальное окружение:
+
+```
+python -m venv env
+```
+
+```
+source env/bin/activate
+```
+
+3) Установить зависимости из файла requirements.txt:
+
+```
+python -m pip install --upgrade pip
+```
+
+```
+pip install -r requirements.txt
+```
+
+4) Выполнить миграции:
+
+```
+python manage.py migrate
+```
+
+5) Запустить проект:
+
+```
+python manage.py runserver
+```
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -6,11 +6,20 @@ User = get_user_model()
 
 class Post(models.Model):
     text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
     image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+        upload_to='posts/',
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.text
@@ -18,9 +27,51 @@ class Post(models.Model):
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
     text = models.TextField()
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        'Дата добавления',
+        auto_now_add=True,
+        db_index=True
+    )
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower'
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('user', 'following'),
+                name='unique_user_following'
+            ),
+        )
+
+    def __str__(self):
+        return f'{self.user} подписан на {self.following}'
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
